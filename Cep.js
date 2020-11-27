2627b6c32481b538d11058fa6c6b40134a797e4f
jojoba10 <ej.carlos676@gmail.com>
2020-11-27 17:39:43 -0300

    Buscador de Cep usando Jquery e Bootstrap

---
+++ Cep.js
@@ -0,0 +1,22 @@
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/"+ cep +"/json/";
+    console.log(url);
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $("#logradouro").html(response.logradouro);
+            $("#uf").html(response.uf);
+            $("#localidade").html(response.localidade);
+            $("#bairro").html(response.bairro);
+            $("#titulo_cep").html("CEP " + response.cep);
+            $(".cep").show();
+        }
+    })
+}
+
+$(function(){
+    $(".cep").hide();
+});
